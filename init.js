3a14072bb68892273b5bd9f70261ea380c6212c9
giphoo <giphoo@vip.qq.com>
2014-12-30 16:09:00 +0800

    1、fix bug

--- init.js
+++ init.js
@@ -1,13 +1,14 @@
 var npm = require("npm");
 
 npm.load({}, function(){
-    var arch = process.arch;
 
-    if(arch.match(/32$/)){
-        npm.install("jsom_win32");
-    }else if(arch.match(/64$/)){
-        npm.install("jsom_win64");
+    if(process.platform.match(/^win/)){
+        if(process.arch.match(/32$/)){
+            npm.install("jsom_win32");
+        }else if(process.arch.match(/64$/)){
+            npm.install("jsom_win64");
+        }
     }else{
         npm.install("jsom");
     }
-})
+});
